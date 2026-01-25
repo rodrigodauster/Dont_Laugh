13231adf37e820d22c041e26d36569e2a178d9bc
Rodrigo Dauster <rdauster@gmail.com>
2026-01-25 10:51:07 +0100

    Initial Commit

---
+++ Dont_Laugh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dont_Laugh
+//
+//  Created by Rodrigo Dauster on 25/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dont_Laugh/Dont_LaughApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dont_LaughApp.swift
+//  Dont_Laugh
+//
+//  Created by Rodrigo Dauster on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Dont_LaughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
